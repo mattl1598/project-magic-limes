d574cabcd7a505f88258a72f450a4e3a641c08d3
Matthew Larby <37371392+mattl1598@users.noreply.github.com>
2022-08-14 08:34:22 +0100

    finalised center detection and starting dmx

--- __main__.py
+++ __main__.py
@@ -1,20 +1,34 @@
 import statistics
 from collections import deque
-
+import platform
+import distinctipy
+from itertools import permutations
 import cv2
-import copy
-import imutils
-from imutils.video import VideoStream
 import time
 import numpy as np
+import math
+
+
+def least_movement(last_pos, new_pos):
+	movements = []
+	perms = list(permutations(new_pos))
+	for i in range(len(perms)):
+		movement = 0
+		for j in range(len(perms[i])):
+			x_change = int(last_pos[j][0] - int(perms[i][j][0]))
+			y_change = int(last_pos[j][1]) - int(perms[i][j][1])
+			movement += int(math.sqrt(abs(x_change**2 + y_change**2)))
+		movements.append(movement)
+
+	return perms[np.argmin(movements)]
 
 
 class AverageSpot:
 	def __init__(self, maxlen):
 		self.cx = deque(maxlen=maxlen)
 		self.cy = deque(maxlen=maxlen)
-		self.r = deque(maxlen=maxlen*2)
-		self.b = deque(maxlen=maxlen*2)
+		self.r = deque(maxlen=maxlen)
+		self.b = deque(maxlen=maxlen)
 
 	def update(self, x, y, r, b):
 		self.cx.append(x)
@@ -28,8 +42,18 @@ class AverageSpot:
 		r = int(sum(self.r)/len(self.r))
 		b = sum(self.b)/len(self.b)
 
-		return (x,y,r,b)
+		return tuple((x, y, r, b))
 
+	def get_last_pos(self):
+		x = self.cx[-1]
+		y = self.cy[-1]
+		r = self.r[-1]
+		b = self.b[-1]
+
+		return tuple((x, y, r, b))
+
+	def get_length(self):
+		return len(self.cx)
 
 class AverageBuffer:
 	def __init__(self, maxlen):
@@ -59,16 +83,26 @@ class WeightedAverageBuffer(AverageBuffer):
 		return mean_frame.astype('uint8')
 
 
-# webcam = cv2.VideoCapture(1)
+if platform.system() != "Windows":
+	webcam = cv2.VideoCapture("/dev/video0")
+	time.sleep(1)
+else:
+	webcam = cv2.VideoCapture("X:\\Silchester Players\\Aladdin\\Footage\\evening full.mp4")
+	time.sleep(1)
+	webcam.set(1, 875)
+	# webcam.set(1, 10000)
 
-webcam = VideoStream(src=2).start()
-# webcam = cv2.VideoCapture("X:\\Silchester Players\\Aladdin\\Footage\\evening full.mp4")
-time.sleep(1)
-# webcam.set(1, 875)
-# webcam.set(1, 10000)
+webcam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+webcam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc(*'MJPG'))
+ret, frame = webcam.read()
+
+# Params
 
 # Background subtraction method (KNN, MOG2)
-algo = "KNN"
+algo = "MOG2"
+spots = 4
+spot_buffer_size = 10
 
 if algo == 'MOG2':
 	backSub = cv2.createBackgroundSubtractorMOG2(history=500, detectShadows=False)
@@ -76,32 +110,48 @@ else:
 	backSub = cv2.createBackgroundSubtractorKNN(history=500, detectShadows=False)
 
 weighted_buffer = AverageBuffer(15)
-
-spots = 2
-spot_buffer_size = 15
 spots_buffers = [AverageSpot(spot_buffer_size) for i in range(spots)]
+
+dots = np.zeros(frame.shape, np.uint8)
+h, w = frame.shape[:2]
+x = np.linspace(0, w, int(w / 10))
+y = np.linspace(0, h, int(h / 10))
+X, Y = np.meshgrid(x, y)
+
+positions = np.column_stack([X.ravel(), Y.ravel()]).astype(int)
+
+for (x, y) in positions:
+	dots[y - 1, x - 1] = [255, 255, 255]
+
+spot_colours = (np.array(distinctipy.get_colors(spots))*255).astype(int).tolist()
+# print(spot_colours)
+last_centers = None
+last_labels = None
+
+gpu = False
+
+if gpu:
+	cv = cv2.cuda
+else:
+	cv = cv2
 
 while True:
 	start = time.time()
 	ret, frame = webcam.read()
-	h, w = frame.shape[:2]
-	if frame is None:
+	if not ret:
 		break
+	h, w = frame.shape[:2]
 
-	# grey = cv2.split(frame)[0]
 	grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	# grey = cv2.split(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV))[2]
 
 	edges = cv2.Canny(grey, 120, 200)
 
 	# Otsu's thresholding after Gaussian filtering
 	blur = cv2.GaussianBlur(edges, (5, 5), 0)
 	ret3, fgThres = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-	blob = fgThres
 
 	frame_f32 = fgThres.astype('float32')
 	weighted_buffer.apply(frame_f32)
-
 	blob = backSub.apply(weighted_buffer.get_frame())
 
 	thresh = cv2.adaptiveThreshold(blob, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 71, 17)
@@ -117,40 +167,19 @@ while True:
 	cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 	mask = np.zeros(grey.shape, np.uint8)
 
-
-
 	height, width = grey.shape
 	min_x, min_y = width, height
 	max_x = max_y = 0
 	rect_num = 0
-	try:
-		area_avg = statistics.mean([cv2.contourArea(cv2.convexHull(c)) for c in cnts])
-	except:
-		area_avg = 0
 
 	for c in cnts:
 		hull = cv2.convexHull(c)
-		# if cv2.contourArea(hull) > 2*area_avg:
 		cv2.drawContours(mask, [hull], -1, (255, 255, 255), -1)
-
 
 	kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 15))
 
 	# Using morph close to get lines outside the drawing
 	mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=3)
-
-	dots = np.zeros(frame.shape, np.uint8)
-	h, w = frame.shape[:2]
-	x = np.linspace(0, w, int(w / 10))
-	y = np.linspace(0, h, int(h / 10))
-
-	X, Y = np.meshgrid(x, y)
-
-	positions = np.column_stack([X.ravel(), Y.ravel()]).astype(int)
-
-	for (x, y) in (positions):
-		# cv2.circle(dots, (x, y), 1, (255, 0, 0), -1)
-		dots[y-1,x-1] = [255,255,255]
 
 	masked_dots = cv2.bitwise_and(dots, dots, mask=mask)
 
@@ -159,27 +188,32 @@ while True:
 
 	criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 	if points.any():
-		ret, label, center = cv2.kmeans(points, spots, None, criteria, 100, cv2.KMEANS_PP_CENTERS)
+		ret, label, center = cv2.kmeans(
+			points,
+			spots,
+			last_labels,
+			criteria,
+			100,
+			cv2.KMEANS_RANDOM_CENTERS
+		)
 	else:
 		center = None
 
 	if center is not None:
 		centers = []
+		new_pos = None
 		for point in center:
 			centers.append(tuple(point.astype(np.uint32)))
-		centers.sort(key=lambda x: x[1])
-		print(centers)
-		cv2.circle(masked_dots, centers[0], 10, (0, 0, 255), -1)
-		cv2.circle(masked_dots, centers[1], 10, (0, 255, 0), -1)
-		# for c in center:
-		# 	print(type(c))q
+		if spots_buffers[0].get_length():
+			last_pos = [spot.get_last_pos()[:2] for spot in spots_buffers]
+			center_indexes = least_movement(last_pos, centers)
+			centers = [centers[i] for i in center_indexes]
+			radii = []
+		for i in range(len(centers)):
+			spots_buffers[i].update(*centers[i], 0, 0)
+			cv2.circle(frame, spots_buffers[i].get_pos()[:2], 10, spot_colours[i], -1)
 
-	# output = cv2.subtract(lights_mask, 255-frame)
-	# output.astype(np.uint8)
-
-	# output += boxes_colour
-
-	output = masked_dots
+	output = frame
 
 	end = time.time()
 	cv2.putText(output, f"{int(1 / (end - start))}FPS", (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
